b25ba37bfeb1cd9c6ccef8e3c2d56d64872122c5
Alejandro Flores <rflores@masteredi.com.mx>
2026-03-24 17:53:57 -0600

    Sucursales

--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import type { KDSOrder, KDSOrderItem } from '../types/types';
+import type { KDSOrder } from '../types/types';
 import { Timestamp } from 'firebase/firestore';
 import toast from 'react-hot-toast';
 
